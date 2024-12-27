77f5e7b38c1220e8c24e328eb3844ca341aa328f
KZT3 <khantzawthit5@gmail.com>
2024-12-27 11:30:22 +0630

    hi

--- src/handlers.js
+++ src/handlers.js
@@ -2,23 +2,23 @@ import Swal from "sweetalert2";
 
 export const alertTestBtnHandler = () => {
   console.log("alert");
-  // Swal.fire({
-  //   title: "Are you sure?",
-  //   text: "You won't be able to revert this!",
-  //   icon: "warning",
-  //   showCancelButton: true,
-  //   confirmButtonColor: "#3085d6",
-  //   cancelButtonColor: "#d33",
-  //   confirmButtonText: "Yes, delete it!"
-  // }).then((result) => {
-  //   if (result.isConfirmed) {
-  //     Swal.fire({
-  //       title: "Deleted!",
-  //       text: "Your file has been deleted.",
-  //       icon: "success"
-  //     });
-  //   }
-  // });
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success",
+      });
+    }
+  });
 
   // const Toast = Swal.mixin({
   //   toast: true,
