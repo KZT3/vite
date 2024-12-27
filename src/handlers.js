bc79344d1fdba17fd288e4774772081644e03a5b
KZT3 <khantzawthit5@gmail.com>
2024-12-27 11:35:59 +0630

    gg

--- src/handlers.js
+++ src/handlers.js
@@ -2,37 +2,37 @@ import Swal from "sweetalert2";
 
 export const alertTestBtnHandler = () => {
   console.log("alert");
-  Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: "Deleted!",
-        text: "Your file has been deleted.",
-        icon: "success",
-      });
-    }
-  });
+  // Swal.fire({
+  //   title: "Are you sure?",
+  //   text: "You won't be able to revert this!",
+  //   icon: "warning",
+  //   showCancelButton: true,
+  //   confirmButtonColor: "#3085d6",
+  //   cancelButtonColor: "#d33",
+  //   confirmButtonText: "Yes, delete it!",
+  // }).then((result) => {
+  //   if (result.isConfirmed) {
+  //     Swal.fire({
+  //       title: "Deleted!",
+  //       text: "Your file has been deleted.",
+  //       icon: "success",
+  //     });
+  //   }
+  // });
 
-  // const Toast = Swal.mixin({
-  //   toast: true,
-  //   position: "center",
-  //   showConfirmButton: false,
-  //   timer: 3000,
-  //   timerProgressBar: true,
-  //   didOpen: (toast) => {
-  //     toast.onmouseenter = Swal.stopTimer;
-  //     toast.onmouseleave = Swal.resumeTimer;
-  //   },
-  // });
-  // Toast.fire({
-  //   icon: "success",
-  //   title: "Done",
-  // });
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "center",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    },
+  });
+  Toast.fire({
+    icon: "success",
+    title: "Done",
+  });
 };
